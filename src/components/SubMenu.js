4adf8f570ce59b5d242d1b447c1099862278ec85
sweta2126 <101868578+sweta2126@users.noreply.github.com>
2022-03-26 15:47:21 +0530

    updated

--- src/components/SubMenu.js
+++ src/components/SubMenu.js
@@ -1,102 +1,56 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components'; 
+import React,{useState} from "react";
 
-const SidebarLink = styled(Link)  ` 
-    display: flex; 
-    color: #e1e9fc; 
-    justify-content: space-between; 
-    align-items: center; 
-    padding: 20px; 
-    list-style: none; 
-    height: 60px; 
-    text-decoration: none; 
-    font-size: 18px; 
-    &:hover { 
-        background: #252831; 
-        border-left: 4px solid #632ce4; 
-        cursor: pointer; 
-    }
-    
-    `; 
-    
-    const SidebarLabel = styled.span` 
-    
-       margin-left: 16px;`
-       
-       ; 
-       
-       const DropdownLink = styled(Link)`
-         background: #414757; 
-         height: 60px; 
-         padding-left: 3rem; 
-         display: flex; 
-         align-items: center; 
-         text-decoration: none; 
-         color: #f5f5f5; 
-         font-size: 18px; 
+ import * as FaIcons from "react-icons/fa";
+ import * as AiIcons from "react-icons/ai"
+ import { Link } from "react-router-dom";
+ import {SidebarData} from './SidebarData';
+ import SubMenu from './SubMenu'
+
+ import "./Sidebar.css";
+
+ function Sidebar(props) {
+     const [sidebar,setSidebar] = useState(false)
+
+     // const showSidebar=()=>setSidebar(!sidebar)
+     const showSidebar = () => {
+       if(sidebar === false){
+         setSidebar(true);
+       }
+       else {
+         setSidebar(false)
+       }
+     }
+  
+   return (
+     <>
+      
+         <div className="navbar">
+           
+           <Link to="/" className="menu-bars">
+             <FaIcons.FaBars onClick={showSidebar} />
+           </Link>
+           <h1><span>{" " + props.title}</span></h1>
+           
+         </div>
+         <nav className={sidebar ? 'nav-menu active' :'nav-menu'}>
          
-         &:hover { 
-             background: #632ce4; 
-             cursor: pointer; 
-            
-            }
-            
-            `; 
-            
-            const SubMenu = ({ item }) => { 
-                const [subnav, setSubnav] = useState(false); 
-                const showSubnav = () => setSubnav(!subnav); 
-                
-                return ( 
-                <> 
-                
-                  <SidebarLink to={item.path} onClick={item.subNav && showSubnav}> 
+             <ul className='nav-menu-items' onClick={showSidebar}>
+               {/* <li className='navbar-toggle'>
+                   <Link to={{state : {fromDashboard:true}}} className="menu-bars">
+                       <AiIcons.AiOutlineClose/>
+                  </Link>
+               </li> */}
+               {SidebarData.map((item, index)=>{
+                 return <SubMenu item={item} key={index} />;
                   
-                  <div> 
-                      {item.icon} 
-                      
-                      <SidebarLabel>{item.title}</SidebarLabel> 
-                      
-                    </div> 
-                    
-                    <div> {
-                    
-                    item.subNav && subnav 
-                    ? item.iconOpened 
-                    : item.subNav 
-                    ? item.iconClosed 
-                    : null} 
-                    
-                    </div> 
-                    
-                    </SidebarLink> 
-                    
-                    
-                    {subnav && 
-                    
-                    item.subNav.map((item, index) => { 
-                        return ( 
-                        
-                        <DropdownLink to={item.path} key={index}> 
-                        
-                        {item.icon} 
-                        
-                        <SidebarLabel>{item.title}</SidebarLabel> 
-                        
-                        </DropdownLink> 
-                        
-                        ); 
-                    
-                    })} 
-                    
-                    
-                    </> 
-                    
-                    );
+               })}
                 
-                }; 
-                
-                
-                
-                export default SubMenu;
+
+             </ul>
+         </nav>
+
+    
+     </>
+   );
+ }
+ export default Sidebar;
